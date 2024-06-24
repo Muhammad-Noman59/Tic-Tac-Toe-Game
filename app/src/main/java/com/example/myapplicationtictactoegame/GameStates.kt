c7fa41aee482eb59ca02aadb90a62860246a6493
Muhammad-Noman59 <muhammadnoman806372@gmail.com>
2024-06-24 12:50:35 +0500

    Game State Create Done

---
+++ app/src/main/java/com/example/myapplicationtictactoegame/GameStates.kt
@@ -0,0 +1,30 @@
+package com.example.myapplicationtictactoegame
+
+data class GameStates (
+    val playerCircleCount : Int = 0,
+    val playerCrossCount : Int = 0,
+    val drawCount : Int = 0,
+    val hintText : String = "Player 'o' Turn",
+    val currentTurn : BordCellValue = BordCellValue.CIRCLE,
+    val hasWon : Boolean = false,
+    val victoryType : VictoryTypeValue = VictoryTypeValue.NONE
+)
+
+enum class BordCellValue{
+    CIRCLE,
+    CROSS,
+    NONE
+}
+
+enum class VictoryTypeValue{
+
+    NONE,
+    HORIZONTAL1,
+    HORIZONTAL2,
+    HORIZONTAL3,
+    VERTICAL1,
+    VERTICAL2,
+    VERTICAL3,
+    DIAGONAL1,
+    DIAGONAL2
+}
